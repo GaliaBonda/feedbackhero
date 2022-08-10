d0534abd100de0b7498faa46840a31615fef0e2a
GaliaBonda <galina.rizhniak@gmail.com>
2022-08-10 14:02:40 +0300

    mobile menu

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -6,11 +6,19 @@ interface Props {
     closeMenu: () => void;
 }
 
-export default function MobileMenu({closeMenu} : Props) {
+export default function MobileMenu({ closeMenu }: Props) {
 
     return (<div className='mobile-menu'>
-        <Nav />
-        <button className='mobile-menu__btn register-btn app-btn'>Register</button>
-        <button className='mobile-menu__btn close-btn app-btn' onClick={closeMenu}>Close</button>
+        <div className="mobile-menu__inner">
+            <Nav />
+            <button className='mobile-menu__btn register-btn app-btn'>Register</button>
+            <button className='mobile-menu__btn close-btn' onClick={closeMenu}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="-1 -1 11 11" version="1.1">
+                    <path fill="#5f6368" stroke="#5f6368" stroke-linecap="round" stroke-width="1.5"
+                        d="m0 0 9,9 M0 9 9,0" />
+                </svg>
+            </button>
+        </div>
+
     </div>);
 }
